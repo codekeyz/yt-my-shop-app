96465ebb2aa01dcdb154dc6a3960b7dfcde77a1b
Cocky Comrade <cockycomrade@gmail.com>
2022-07-20 17:45:28 +0000

    add category & product model

---
+++ lib/model/category.dart
@@ -0,0 +1,6 @@
+class Category {
+  final String name;
+  final int id;
+
+  const Category(this.name, this.id);
+}
--- lib/model/product.dart
+++ lib/model/product.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 class Product {
   final String name;
+  final String? description;
   final double price;
   final ImageProvider image;
 
-  const Product(this.name, this.price, this.image);
+  const Product(
+    this.name,
+    this.price,
+    this.image, {
+    this.description,
+  });
 }
